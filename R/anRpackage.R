902768d1f51af4de9113ffe1020787d4f9a7e9af
Sherwood <bsherwood@gmail.com>
2024-02-29 17:23:53 -0500

    Initial commit

---
+++ R/anRpackage.R
@@ -0,0 +1,14 @@
+#' @importFrom Rcpp evalCpp
+#' @useDynLib anRpackage, .registration=TRUE
+NULL 
+
+#' anRpackage: Testing to see if I can get Rcpp Armadillo to work with other basic 
+#' r functions for a package. 
+#' 
+#' @section anRpackage functions:
+#' writing some functions
+#'  
+#' @docType package
+#' @name anRpackage
+#' @aliases anRpackage-package
+NULL
---
+++ R/otherFile.R
@@ -0,0 +1,12 @@
+#' Add together two numbers
+#'
+#' @param x A number.
+#' @param y A number.
+#' @return A number.
+#' @examples
+#' add(1, 1)
+#' add(10, 1)
+#' @export
+add <- function(x, y) {
+  addTwoNumbers(x,y)
+}
