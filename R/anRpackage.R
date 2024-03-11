db1d1aa980a80fc83897ad5b8fbbfa9395fdabf4
Sherwood <bsherwood@gmail.com>
2024-03-11 10:46:30 -0400

    Update anRpackage.R

--- R/anRpackage.R
+++ R/anRpackage.R
@@ -1,5 +1,17 @@
+#' @import quantreg
+#' @import hqreg
+#' @import plyr
+#' @import hrqglas
+#' @import data.table
+#' @import lifecycle
+#' @import Matrix
+#' @importFrom splines bs
+#' @importFrom graphics lines plot par segments points legend
+#' @importFrom stats coef coefficients predict quantile residuals sd xtabs fitted weighted.mean IQR resid
+#' @importFrom Rdpack reprompt
+#' @importFrom methods is
 #' @importFrom Rcpp evalCpp
-#' @useDynLib anRpackage, .registration=TRUE
+#' @useDynLib rqPen, .registration=TRUE
 NULL 
 
 #' anRpackage: Testing to see if I can get Rcpp Armadillo to work with other basic 
